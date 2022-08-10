ece611e030c5b33c0d945107d54371a5bb046715
astream <positive.thought1009@gmail.com>
2022-08-10 18:31:52 +0900

    implemeted google logon function

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,21 @@
+import { signInWithPopup } from 'firebase/auth'
 import React from 'react'
+import { auth, provider } from '../firebase';
+//import { auth, provider } from '../firebase';
+
+const loginInWithGoogle = () => {
+  signInWithPopup(auth, provider).then((result) => {
+
+  });
+};
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+      <p>ログインして始める</p>
+      <button onClick={loginInWithGoogle}>Googleでログイン</button>
+      
+    </div>
   )
 }
 
